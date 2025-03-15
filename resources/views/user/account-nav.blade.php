247c67b6d93b8022bf78155c00d386c5afa3e6bc
Evan Agustian Lukius <71422920+evanlukius@users.noreply.github.com>
2025-03-15 13:30:13 +0900

    update

--- resources/views/user/account-nav.blade.php
+++ resources/views/user/account-nav.blade.php
@@ -5,9 +5,11 @@
             <li><a href="account-details.html" class="menu-link menu-link_us-s">Account Details</a></li>
             <li><a href="account-wishlist.html" class="menu-link menu-link_us-s">Wishlist</a></li>
             <li>
-                <form method="POST" action="{{ route('logout') }}" id="logout-form">
+                <form method="POST" action="{{ route('logout')}}" id="logout-form">
                     @csrf
-                <a href="{{ route('logout') }}" class="menu-link menu-link_us-s" onclick="document.getElementById('logout-form').submit();">Logout</a>
+                <a href="{{ route('logout')}}"class="" onclick="event.preventDefault();document.getElementById('logout-form').submit();"> 
+                    Logout
+                </a>
                 </form>
             </li>
 </ul>
